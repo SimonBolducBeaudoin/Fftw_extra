874d7aebea5fa4ebb25e9e455feed6a99bdeebec
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2023-09-16 11:40:07 -0400

    Rm AutoGitVersion

--- src/python_submodule.cpp
+++ src/python_submodule.cpp
@@ -1,5 +1,4 @@
 #include "python_submodule.h"
-#include "git_version.h"
 
 // See Pybind11 FAQ «How can I reduce the build time ?» :
 // https://pybind11.readthedocs.io/en/stable/faq.html#how-can-i-reduce-the-build-time
@@ -7,10 +6,7 @@
 //Python Binding and Time_Quad class instances.
 PYBIND11_MODULE(fftw_extra, m)
 {
-    m.doc() = "Extra function related to fftw library.\n"\
-    "Git Info : \n "\
-    + std::string(kGitInfo)\
-    +"\n";   
+    m.doc() = "Extra function related to fftw library.\n";   
 	init_fftw_extra(m);
 }
 
